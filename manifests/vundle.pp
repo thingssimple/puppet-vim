8b6a14fc92e67cbca56949ee4ee51d9ffb09855a
Michael Avila <me@michaelavila.com>
2015-12-24 22:45:46 -0800

    Change to only install Vundle

--- manifests/vundle.pp
+++ manifests/vundle.pp
@@ -8,26 +8,7 @@ class vim::vundle {
     ensure => directory
   }
 
-  file { "${home}/.vimrc":
-    ensure => present
-  }
-
   repository { "${home}/.vim/bundle/Vundle.vim":
     source => "VundleVim/Vundle.vim"
-  }
-
-  file_line { "Add vundle to rtp":
-    path => "${home}/.vimrc",
-    line => "set rtp+=~/.vim/bundle/Vundle.vim"
-  }
-
-  file_line { "Begin vundle":
-    path => "${home}/.vimrc",
-    line => "call vundle#begin()"
-  }
-
-  file_line { "End vundle":
-    path => "${home}/.vimrc",
-    line => "call vundle#end()"
   }
 }
