d6b5bb66ae352b1b8b102a07ba68afde29354352
Michael Avila <me@michaelavila.com>
2015-12-24 19:38:30 -0800

    Ensure Vundle is configured

--- manifests/vundle.pp
+++ manifests/vundle.pp
@@ -11,4 +11,19 @@ class vim::vundle {
   repository { "${home}/.vim/bundle/Vundle.vim":
     source => "VundleVim/Vundle.vim"
   }
+
+  file_line { "Add vundle to rtp":
+    path => "${home}/.vimrc",
+    line => "set rtp+=~/.vim/bundle/Vundle.vim"
+  }
+
+  file_line { "Begin vundle":
+    path => "${home}/.vimrc",
+    line => "call vundle#begin()"
+  }
+
+  file_line { "End vundle":
+    path => "${home}/.vimrc",
+    line => "call vundle#end()"
+  }
 }
